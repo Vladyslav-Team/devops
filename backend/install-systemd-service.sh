10087d665e62fd3f20a468434ee63c155f9b9684
ViktorVoloshko <viktor.voloshko@outlook.com>
2023-03-23 04:08:44 +0200

    Initial project-oriented pipeline

--- backend/install-systemd-service.sh
+++ backend/install-systemd-service.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -f /etc/systemd/system/UpTalentBackend.service ]; then
+if [ -f /etc/systemd/system/skillscope.service ]; then
     echo "systemd unit already installed!"
 else
-    sudo cp ./backend/UpTalentBackend.service /etc/systemd/system/UpTalentBackend.service
+    sudo cp ./backend/skillscope.service /etc/systemd/system/skillscope.service
     sudo systemctl daemon-reload
     echo "systemd unit installed!"
 fi
